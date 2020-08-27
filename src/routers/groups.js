c18706dcc80657707aaca80ca88b15a30479f286
amandreasen <apa49@cornell.edu>
2020-08-26 21:12:55 -0400

    added group get routes

--- src/routers/groups.js
+++ src/routers/groups.js
@@ -4,7 +4,25 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('hello!')
+    query = 'SELECT * FROM groups;'
+    pool.query(query, (e, result) => {
+        if(e){
+            return res.status(400).send(e)
+        }
+        res.send(result.rows)
+    })
+});
+
+router.get('/:id', (req, res) => {
+    query = `SELECT * FROM groups WHERE group_id=${req.params.id}`;
+    pool.query(query, (e, result) => {
+        if(e){
+            return res.status(400).send(e)
+        } else if (res.length === 0){
+            return res.status(404).send('Group not found!')
+        }
+        res.send(result.rows)
+    })
 })
 
 module.exports = router
