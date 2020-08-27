c0fd6626adc21ca92cf00bc4eac403fffadbd632
amandreasen <apa49@cornell.edu>
2020-08-27 16:44:29 -0400

    updated pool connection

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,6 +1,4 @@
 const {Pool} = require('pg')
-const url = require('url')
-
 // const pool = new Pool({
 //     user: process.env.DB_USER,
 //     host: process.env.DB_HOST,
@@ -9,17 +7,11 @@ const url = require('url')
 //     port: process.env.DB_PORT
 // })
 
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':')
 
 const pool = new Pool({
-    user: auth[0],
-    password: auth[1],
-    host: params.hostname,
-    port: params.port,
-    database: params.pathname.split('/')[1]
+    connectionString=process.env.DATABASE_URL
 })
 
-module.exports = {pool}
+
 
 
