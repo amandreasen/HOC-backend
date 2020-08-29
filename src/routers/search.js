6a5ddd4cc91515dae02fe2b00790a6c329700d03
amandreasen <apa49@cornell.edu>
2020-08-29 01:19:06 -0400

    added search by term

--- src/routers/search.js
+++ src/routers/search.js
@@ -3,56 +3,17 @@ const {pool} = require('../db/pool')
 var express = require('express');
 var router = express.Router();
 
+router.get('/', async (req, res) => {
+    let term = req.query.term === undefined ? '' : req.query.term;
+    term = term.split(' ').join('&');
+    param = [term];
 
-//search for groups with specified tag(provide tag_id); optionally provide limit parameter to limit results
-router.get('/', (req, res) =>{
-    query = `SELECT g.group_id, g.name, g.description, g.open, g.public
-    FROM groups g, join_groups_tags j
-    WHERE j.tag_id=${req.query.tag_id} AND g.group_id=j.group_id`
+    const query = `SELECT * FROM groups WHERE text_vector @@ to_tsquery($1);`
 
-    console.log(req.query)
-    query = req.query.limit === undefined ? query + ';' : query + ` LIMIT ${req.query.limit};` 
+    await pool.query(query,param)
+        .then(result => res.send(result.rows))
+        .catch(e => res.status(400).send(e))
+});
 
-    pool.query(query)
-        .then(result => {
-            data = result.rows 
-            if (data.length === 0){
-                return res.status(404).send('No groups found!')
-            }
-            res.send(data)
-        })
-        .catch(e => res.send(e))
-})
-
-// router.get('/', (req, res) => {
-//     query = `SELECT g.group_id, g.name, g.description, g.open, g.public
-//     FROM groups g, join_groups_tags j`
-
-//     params = req.query
-//     if(params !== {}){
-//         query = query + ' WHERE'
-
-//         if(params.tag_id){
-//             query = query + ` j.tag_id=${req.query.tag_id} AND g.group_id=j.group_id`
-//             query = params.term === undefined ? query : query + ' AND'
-//         }
-
-//         if(params.term){
-//             query = ` g.name LIKE ${params.term}% OR g.description LIKE ${params.term}%`
-//         }
-//     }
-
-//     query = query + ';';
-
-//     pool.query(query)
-//     .then(result => {
-//         data = result.rows 
-//         if (data.length === 0){
-//             return res.status(404).send('No groups found!')
-//         }
-//         res.send(data)
-//     })
-//     .catch(e => res.send(e))
-// })
 
 module.exports = router
